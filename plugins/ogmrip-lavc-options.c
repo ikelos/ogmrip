c81d7f2eb907debf7b41202c8cab4ee9d52d6451
Olivier Rolland <billl@users.sourceforge.net>
2011-09-26 23:39:27 +0200

    Added missing options

--- plugins/ogmrip-lavc-options.c
+++ plugins/ogmrip-lavc-options.c
@@ -48,25 +48,29 @@ struct _OGMRipLavcDialog
 
   OGMRipProfile *profile;
 
-  GtkWidget *mv0_check;
-  GtkWidget *v4mv_check;
-  GtkWidget *mbd_combo;
-  GtkWidget *strict_combo;
-  GtkWidget *vb_strategy_combo;
-  GtkWidget *qns_combo;
+  GtkWidget *buf_size_spin;
+  GtkWidget *cmp_spin;
+  GtkWidget *dc_spin;
+  GtkWidget *dia_spin;
+  GtkWidget *grayscale_check;
   GtkWidget *keyint_spin;
   GtkWidget *last_pred_spin;
-  GtkWidget *vqcomp_spin;
-  GtkWidget *dc_spin;
-  GtkWidget *preme_combo;
-  GtkWidget *cmp_spin;
-  GtkWidget *precmp_spin;
-  GtkWidget *subcmp_spin;
-  GtkWidget *dia_spin;
-  GtkWidget *predia_spin;
-  GtkWidget *buf_size_spin;
-  GtkWidget *min_rate_spin;
+  GtkWidget *max_bframes_spin;
   GtkWidget *max_rate_spin;
+  GtkWidget *mbd_combo;
+  GtkWidget *min_rate_spin;
+  GtkWidget *mv0_check;
+  GtkWidget *precmp_spin;
+  GtkWidget *predia_spin;
+  GtkWidget *preme_combo;
+  GtkWidget *qns_combo;
+  GtkWidget *qpel_check;
+  GtkWidget *strict_combo;
+  GtkWidget *subcmp_spin;
+  GtkWidget *trellis_check;
+  GtkWidget *v4mv_check;
+  GtkWidget *vb_strategy_combo;
+  GtkWidget *vqcomp_spin;
 };
 
 struct _OGMRipLavcDialogClass
@@ -97,44 +101,52 @@ ogmrip_lavc_dialog_set_profile (OGMRipLavcDialog *dialog, OGMRipProfile *profile
   settings = ogmrip_profile_get_child (profile, "lavc");
   g_assert (settings != NULL);
 
-  g_settings_bind (settings, OGMRIP_LAVC_PROP_MV0,
-      dialog->mv0_check, "active", G_SETTINGS_BIND_DEFAULT);
-  g_settings_bind (settings, OGMRIP_LAVC_PROP_V4MV,
-      dialog->v4mv_check, "active", G_SETTINGS_BIND_DEFAULT);
-  g_settings_bind (settings, OGMRIP_LAVC_PROP_MBD,
-      dialog->mbd_combo, "active", G_SETTINGS_BIND_DEFAULT);
-  g_settings_bind (settings, OGMRIP_LAVC_PROP_STRICT,
-      dialog->strict_combo, "active", G_SETTINGS_BIND_DEFAULT);
-  g_settings_bind (settings, OGMRIP_LAVC_PROP_VB_STRATEGY,
-      dialog->vb_strategy_combo, "active", G_SETTINGS_BIND_DEFAULT);
-  g_settings_bind (settings, OGMRIP_LAVC_PROP_QNS,
-      dialog->qns_combo, "active", G_SETTINGS_BIND_DEFAULT);
+  g_settings_bind (settings, OGMRIP_LAVC_PROP_BUF_SIZE,
+      dialog->buf_size_spin, "value", G_SETTINGS_BIND_DEFAULT);
+  g_settings_bind (settings, OGMRIP_LAVC_PROP_CMP,
+      dialog->cmp_spin, "value", G_SETTINGS_BIND_DEFAULT);
+  g_settings_bind (settings, OGMRIP_LAVC_PROP_DC,
+      dialog->dc_spin, "value", G_SETTINGS_BIND_DEFAULT);
+  g_settings_bind (settings, OGMRIP_LAVC_PROP_DIA,
+      dialog->dia_spin, "value", G_SETTINGS_BIND_DEFAULT);
+  g_settings_bind (settings, OGMRIP_LAVC_PROP_GRAYSCALE,
+      dialog->grayscale_check, "active", G_SETTINGS_BIND_DEFAULT);
   g_settings_bind (settings, OGMRIP_LAVC_PROP_KEYINT,
       dialog->keyint_spin, "value", G_SETTINGS_BIND_DEFAULT);
   g_settings_bind (settings, OGMRIP_LAVC_PROP_LAST_PRED,
       dialog->last_pred_spin, "value", G_SETTINGS_BIND_DEFAULT);
-  g_settings_bind (settings, OGMRIP_LAVC_PROP_VQCOMP,
-      dialog->vqcomp_spin, "value", G_SETTINGS_BIND_DEFAULT);
-  g_settings_bind (settings, OGMRIP_LAVC_PROP_DC,
-      dialog->dc_spin, "value", G_SETTINGS_BIND_DEFAULT);
-  g_settings_bind (settings, OGMRIP_LAVC_PROP_PREME,
-      dialog->preme_combo, "active", G_SETTINGS_BIND_DEFAULT);
-  g_settings_bind (settings, OGMRIP_LAVC_PROP_CMP,
-      dialog->cmp_spin, "value", G_SETTINGS_BIND_DEFAULT);
-  g_settings_bind (settings, OGMRIP_LAVC_PROP_PRECMP,
-      dialog->precmp_spin, "value", G_SETTINGS_BIND_DEFAULT);
-  g_settings_bind (settings, OGMRIP_LAVC_PROP_SUBCMP,
-      dialog->subcmp_spin, "value", G_SETTINGS_BIND_DEFAULT);
-  g_settings_bind (settings, OGMRIP_LAVC_PROP_DIA,
-      dialog->dia_spin, "value", G_SETTINGS_BIND_DEFAULT);
-  g_settings_bind (settings, OGMRIP_LAVC_PROP_PREDIA,
-      dialog->predia_spin, "value", G_SETTINGS_BIND_DEFAULT);
-  g_settings_bind (settings, OGMRIP_LAVC_PROP_BUF_SIZE,
-      dialog->buf_size_spin, "value", G_SETTINGS_BIND_DEFAULT);
-  g_settings_bind (settings, OGMRIP_LAVC_PROP_MIN_RATE,
-      dialog->min_rate_spin, "value", G_SETTINGS_BIND_DEFAULT);
+  g_settings_bind (settings, OGMRIP_LAVC_PROP_MAX_BFRAMES,
+      dialog->max_bframes_spin, "value", G_SETTINGS_BIND_DEFAULT);
   g_settings_bind (settings, OGMRIP_LAVC_PROP_MAX_RATE,
       dialog->max_rate_spin, "value", G_SETTINGS_BIND_DEFAULT);
+  g_settings_bind (settings, OGMRIP_LAVC_PROP_MBD,
+      dialog->mbd_combo, "active", G_SETTINGS_BIND_DEFAULT);
+  g_settings_bind (settings, OGMRIP_LAVC_PROP_MIN_RATE,
+      dialog->min_rate_spin, "value", G_SETTINGS_BIND_DEFAULT);
+  g_settings_bind (settings, OGMRIP_LAVC_PROP_MV0,
+      dialog->mv0_check, "active", G_SETTINGS_BIND_DEFAULT);
+  g_settings_bind (settings, OGMRIP_LAVC_PROP_PRECMP,
+      dialog->precmp_spin, "value", G_SETTINGS_BIND_DEFAULT);
+  g_settings_bind (settings, OGMRIP_LAVC_PROP_PREDIA,
+      dialog->predia_spin, "value", G_SETTINGS_BIND_DEFAULT);
+  g_settings_bind (settings, OGMRIP_LAVC_PROP_PREME,
+      dialog->preme_combo, "active", G_SETTINGS_BIND_DEFAULT);
+  g_settings_bind (settings, OGMRIP_LAVC_PROP_QNS,
+      dialog->qns_combo, "active", G_SETTINGS_BIND_DEFAULT);
+  g_settings_bind (settings, OGMRIP_LAVC_PROP_QPEL,
+      dialog->qpel_check, "active", G_SETTINGS_BIND_DEFAULT);
+  g_settings_bind (settings, OGMRIP_LAVC_PROP_STRICT,
+      dialog->strict_combo, "active", G_SETTINGS_BIND_DEFAULT);
+  g_settings_bind (settings, OGMRIP_LAVC_PROP_SUBCMP,
+      dialog->subcmp_spin, "value", G_SETTINGS_BIND_DEFAULT);
+  g_settings_bind (settings, OGMRIP_LAVC_PROP_TRELLIS,
+      dialog->trellis_check, "active", G_SETTINGS_BIND_DEFAULT);
+  g_settings_bind (settings, OGMRIP_LAVC_PROP_V4MV,
+      dialog->v4mv_check, "active", G_SETTINGS_BIND_DEFAULT);
+  g_settings_bind (settings, OGMRIP_LAVC_PROP_VB_STRATEGY,
+      dialog->vb_strategy_combo, "active", G_SETTINGS_BIND_DEFAULT);
+  g_settings_bind (settings, OGMRIP_LAVC_PROP_VQCOMP,
+      dialog->vqcomp_spin, "value", G_SETTINGS_BIND_DEFAULT);
 
   g_object_unref (settings);
 }
@@ -209,25 +221,29 @@ ogmrip_lavc_dialog_init (OGMRipLavcDialog *dialog)
   gtk_container_add (GTK_CONTAINER (misc), widget);
   gtk_widget_show (widget);
 
-  dialog->mv0_check = gtk_builder_get_widget (builder, "mv0-check");
-  dialog->v4mv_check = gtk_builder_get_widget (builder, "v4mv-check");
-  dialog->mbd_combo = gtk_builder_get_widget (builder, "mbd-combo");
-  dialog->strict_combo = gtk_builder_get_widget (builder, "strict-combo");
-  dialog->vb_strategy_combo = gtk_builder_get_widget (builder, "vb_strategy-combo");
-  dialog->qns_combo = gtk_builder_get_widget (builder, "qns-combo");
+  dialog->buf_size_spin = gtk_builder_get_widget (builder, "buf_size-spin");
+  dialog->cmp_spin = gtk_builder_get_widget (builder, "cmp-spin");
+  dialog->dc_spin = gtk_builder_get_widget (builder, "dc-spin");
+  dialog->dia_spin = gtk_builder_get_widget (builder, "dia-spin");
+  dialog->grayscale_check = gtk_builder_get_widget (builder, "grayscale-check");
   dialog->keyint_spin = gtk_builder_get_widget (builder, "keyint-spin");
   dialog->last_pred_spin = gtk_builder_get_widget (builder, "last_pred-spin");
-  dialog->vqcomp_spin = gtk_builder_get_widget (builder, "vqcomp-spin");
-  dialog->dc_spin = gtk_builder_get_widget (builder, "dc-spin");
-  dialog->preme_combo = gtk_builder_get_widget (builder, "preme-combo");
-  dialog->cmp_spin = gtk_builder_get_widget (builder, "cmp-spin");
-  dialog->precmp_spin = gtk_builder_get_widget (builder, "precmp-spin");
-  dialog->subcmp_spin = gtk_builder_get_widget (builder, "subcmp-spin");
-  dialog->dia_spin = gtk_builder_get_widget (builder, "dia-spin");
-  dialog->predia_spin = gtk_builder_get_widget (builder, "predia-spin");
-  dialog->buf_size_spin = gtk_builder_get_widget (builder, "buf_size-spin");
-  dialog->min_rate_spin = gtk_builder_get_widget (builder, "min_rate-spin");
+  dialog->max_bframes_spin = gtk_builder_get_widget (builder, "max_bframes-spin");
   dialog->max_rate_spin = gtk_builder_get_widget (builder, "max_rate-spin");
+  dialog->mbd_combo = gtk_builder_get_widget (builder, "mbd-combo");
+  dialog->min_rate_spin = gtk_builder_get_widget (builder, "min_rate-spin");
+  dialog->mv0_check = gtk_builder_get_widget (builder, "mv0-check");
+  dialog->precmp_spin = gtk_builder_get_widget (builder, "precmp-spin");
+  dialog->predia_spin = gtk_builder_get_widget (builder, "predia-spin");
+  dialog->preme_combo = gtk_builder_get_widget (builder, "preme-combo");
+  dialog->qns_combo = gtk_builder_get_widget (builder, "qns-combo");
+  dialog->qpel_check = gtk_builder_get_widget (builder, "qpel-check");
+  dialog->strict_combo = gtk_builder_get_widget (builder, "strict-combo");
+  dialog->subcmp_spin = gtk_builder_get_widget (builder, "subcmp-spin");
+  dialog->trellis_check = gtk_builder_get_widget (builder, "trellis-check");
+  dialog->v4mv_check = gtk_builder_get_widget (builder, "v4mv-check");
+  dialog->vb_strategy_combo = gtk_builder_get_widget (builder, "vb_strategy-combo");
+  dialog->vqcomp_spin = gtk_builder_get_widget (builder, "vqcomp-spin");
 
   g_object_unref (builder);
 }
