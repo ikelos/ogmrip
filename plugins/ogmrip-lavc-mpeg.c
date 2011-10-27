57b6c9974429ae77cc7e10546ef1b32918baf4ef
Olivier Rolland <billl@users.sourceforge.net>
2011-10-27 18:49:35 +0200

    Added mpeg1 and mpeg2 support

---
+++ plugins/ogmrip-lavc-mpeg.c
@@ -0,0 +1,172 @@
+/* OGMRip - A library for DVD ripping and encoding
+ * Copyright (C) 2004-2011 Olivier Rolland <billl@users.sourceforge.net>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <ogmrip-lavc.h>
+#include <ogmrip-encode.h>
+#include <ogmrip-module.h>
+
+#include <glib/gi18n-lib.h>
+
+typedef struct _OGMRipLavcMpeg1      OGMRipLavcMpeg1;
+typedef struct _OGMRipLavcMpeg1Class OGMRipLavcMpeg1Class;
+
+struct _OGMRipLavcMpeg1
+{
+  OGMRipLavc parent_instance;
+};
+
+struct _OGMRipLavcMpeg1Class
+{
+  OGMRipLavcClass parent_class;
+};
+
+G_DEFINE_TYPE (OGMRipLavcMpeg1, ogmrip_lavc_mpeg1, OGMRIP_TYPE_LAVC)
+
+const gchar *
+ogmrip_lavc_mpeg1_get_codec (void)
+{
+  const gchar *codec = "mpeg1video";
+
+  return codec;
+}
+
+static void
+ogmrip_lavc_mpeg1_class_init (OGMRipLavcMpeg1Class *klass)
+{
+  OGMRipLavcClass *lavc_class;
+
+  lavc_class = (OGMRipLavcClass *) klass;
+  lavc_class->get_codec = ogmrip_lavc_mpeg1_get_codec;
+}
+
+static void
+ogmrip_lavc_mpeg1_init (OGMRipLavcMpeg1 *lavc_mpeg1)
+{
+}
+
+typedef struct _OGMRipLavcMpeg2      OGMRipLavcMpeg2;
+typedef struct _OGMRipLavcMpeg2Class OGMRipLavcMpeg2Class;
+
+struct _OGMRipLavcMpeg2
+{
+  OGMRipLavc parent_instance;
+};
+
+struct _OGMRipLavcMpeg2Class
+{
+  OGMRipLavcClass parent_class;
+};
+
+static gint ogmrip_lavc_mpeg2_run (OGMJobSpawn *spawn);
+
+G_DEFINE_TYPE (OGMRipLavcMpeg2, ogmrip_lavc_mpeg2, OGMRIP_TYPE_LAVC)
+
+const gchar *
+ogmrip_lavc_mpeg2_get_codec (void)
+{
+  const gchar *codec = "mpeg2video";
+
+  return codec;
+}
+
+static void
+ogmrip_lavc_mpeg2_class_init (OGMRipLavcMpeg2Class *klass)
+{
+  OGMJobSpawnClass *spawn_class;
+  OGMRipLavcClass *lavc_class;
+
+  spawn_class = (OGMJobSpawnClass *) klass;
+  spawn_class->run = ogmrip_lavc_mpeg2_run;
+
+  lavc_class = (OGMRipLavcClass *) klass;
+  lavc_class->get_codec = ogmrip_lavc_mpeg2_get_codec;
+}
+
+static void
+ogmrip_lavc_mpeg2_init (OGMRipLavcMpeg2 *lavc_mpeg2)
+{
+}
+
+static gint
+ogmrip_lavc_mpeg2_run (OGMJobSpawn *spawn)
+{
+  ogmrip_lavc_set_v4mv (OGMRIP_LAVC (spawn), FALSE);
+
+  return OGMJOB_SPAWN_CLASS (ogmrip_lavc_mpeg2_parent_class)->run (spawn);
+}
+
+typedef struct _OGMRipLavcMpeg4      OGMRipLavcMpeg4;
+typedef struct _OGMRipLavcMpeg4Class OGMRipLavcMpeg4Class;
+
+struct _OGMRipLavcMpeg4
+{
+  OGMRipLavc parent_instance;
+};
+
+struct _OGMRipLavcMpeg4Class
+{
+  OGMRipLavcClass parent_class;
+};
+
+G_DEFINE_TYPE (OGMRipLavcMpeg4, ogmrip_lavc_mpeg4, OGMRIP_TYPE_LAVC)
+
+static void
+ogmrip_lavc_mpeg4_class_init (OGMRipLavcMpeg4Class *klass)
+{
+}
+
+static void
+ogmrip_lavc_mpeg4_init (OGMRipLavcMpeg4 *lavc_mpeg4)
+{
+}
+
+void
+ogmrip_module_load (OGMRipModule *module)
+{
+  gchar *output;
+  gboolean match;
+
+  if (!g_spawn_command_line_sync ("mencoder -ovc help", &output, NULL, NULL, NULL))
+  {
+    g_warning (_("MEncoder is missing"));
+    return;
+  }
+
+  match = g_regex_match_simple ("^ *lavc *- .*$", output, G_REGEX_MULTILINE, 0);
+  g_free (output);
+
+  if (!match)
+  {
+    g_warning (_("MEncoder is built without LAVC support"));
+    return;
+  }
+
+  ogmrip_register_codec (ogmrip_lavc_mpeg1_get_type (),
+      "lavc-mpeg1", _("Lavc Mpeg-1"), OGMRIP_FORMAT_MPEG1);
+
+  ogmrip_register_codec (ogmrip_lavc_mpeg2_get_type (),
+      "lavc-mpeg2", _("Lavc Mpeg-2"), OGMRIP_FORMAT_MPEG2);
+
+  ogmrip_register_codec (ogmrip_lavc_mpeg4_get_type (),
+      "lavc-mpeg4", _("Lavc Mpeg-4"), OGMRIP_FORMAT_MPEG4);
+}
+
