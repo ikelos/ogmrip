9aa6a9d5ab03ab810d1527c4734171f610e7a24e
Olivier Rolland <billl@users.sourceforge.net>
2012-02-24 13:52:33 +0100

    Fixed compilation with latest libmediainfo

--- libogmrip-file/ogmrip-media-info.c
+++ libogmrip-file/ogmrip-media-info.c
@@ -20,6 +20,11 @@
 
 #include <ogmrip-media.h>
 
+#if defined(UNICODE) || defined (_UNICODE)
+#include <string.h>
+#include <wchar.h>
+#endif
+
 #define MEDIAINFO_GLIBC
 #include <MediaInfoDLL/MediaInfoDLL.h>
 
@@ -28,12 +33,53 @@ struct _OGMRipMediaInfoPriv
   const gchar *filename;
   gboolean is_open;
   gpointer handle;
+  gchar *value;
 };
 
 static OGMRipMediaInfo *default_media_info = NULL;
 
 static void ogmrip_media_info_constructed  (GObject *gobject);
 static void ogmrip_media_info_finalize     (GObject *gobject);
+
+#if defined(UNICODE) || defined (_UNICODE)
+static gchar *
+g_locale_from_wstring (const wchar_t *wstr)
+{
+  mbstate_t ps;
+  size_t len;
+  gchar *str;
+
+  memset (&ps, 0, sizeof (ps));
+  len = wcsrtombs (NULL, &wstr, 0, &ps);
+
+  if (len == (size_t) -1)
+    return NULL;
+
+  str = g_new0 (char, len + 1);
+  wcsrtombs (str, &wstr, len + 1, &ps);
+
+  return str;
+}
+
+static wchar_t *
+g_locale_to_wstring (const gchar *str)
+{
+  mbstate_t ps;
+  size_t len;
+  wchar_t *wstr;
+
+  memset (&ps, 0, sizeof (ps));
+  len = mbsrtowcs (NULL, &str, 0, &ps);
+
+  if (len == (size_t) -1)
+    return NULL;
+
+  wstr = g_new0 (wchar_t, len + 1);
+  mbsrtowcs (wstr, &str, len + 1, &ps);
+
+  return wstr;
+}
+#endif
 
 G_DEFINE_TYPE (OGMRipMediaInfo, ogmrip_media_info, G_TYPE_OBJECT)
 
@@ -75,6 +121,8 @@ ogmrip_media_info_finalize (GObject *gobject)
 {
   OGMRipMediaInfo *info = OGMRIP_MEDIA_INFO (gobject);
 
+  g_free (info->priv->value);
+
   if (info->priv->handle)
   {
     if (info->priv->is_open)
@@ -107,7 +155,15 @@ ogmrip_media_info_open (OGMRipMediaInfo *info, const gchar *filename)
   if (!g_file_test (filename, G_FILE_TEST_IS_REGULAR))
     return FALSE;
 
+#if defined(UNICODE) || defined (_UNICODE)
+  wchar_t *wstr;
+
+  wstr = g_locale_to_wstring (filename);
+  info->priv->is_open = MediaInfo_Open (info->priv->handle, wstr);
+  g_free (wstr);
+#else
   info->priv->is_open = MediaInfo_Open (info->priv->handle, filename);
+#endif
 
   return info->priv->is_open;
 }
@@ -135,20 +191,22 @@ ogmrip_media_info_get (OGMRipMediaInfo *info, OGMRipCategoryType category, guint
   if (!info->priv->handle)
     return NULL;
 
+#if defined(UNICODE) || defined (_UNICODE)
+  wchar_t *wstr1;
+  const wchar_t *wstr2;
+
+  wstr1 = g_locale_to_wstring (name);
+  wstr2 = MediaInfo_Get (info->priv->handle, category, stream, wstr1, MediaInfo_Info_Text, MediaInfo_Info_Name);
+  g_free (wstr1);
+
+  if (info->priv->value)
+    g_free (info->priv->value);
+
+  value = info->priv->value = g_locale_from_wstring (wstr2);
+#else
   value = MediaInfo_Get (info->priv->handle, category, stream, name, MediaInfo_Info_Text, MediaInfo_Info_Name);
+#endif
 
   return *value ? value : NULL;
-}
-
-const gchar *
-ogmrip_media_info_options (OGMRipMediaInfo *info, const gchar *name, const gchar *value)
-{
-  g_return_val_if_fail (OGMRIP_IS_MEDIA_INFO (info), NULL);
-  g_return_val_if_fail (name != NULL, NULL);
-
-  if (!info->priv->handle)
-    return NULL;
-
-  return MediaInfo_Option (info->priv->handle, name, value);
 }
 
