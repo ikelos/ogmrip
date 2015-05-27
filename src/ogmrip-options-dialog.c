cd2b7d0ed151110da29efb021fa759040b43ee10
Olivier Rolland <billl@users.sourceforge.net>
2015-05-27 21:19:54 +0200

    Disabled compressibility test when not sensitive

--- src/ogmrip-options-dialog.c
+++ src/ogmrip-options-dialog.c
@@ -857,7 +857,7 @@ ogmrip_options_dialog_set_test (OGMRipOptionsDialog *dialog, gboolean do_check)
 {
   g_return_if_fail (OGMRIP_IS_OPTIONS_DIALOG (dialog));
 
-  gtk_toggle_button_set_active (GTK_TOGGLE_BUTTON (dialog->priv->test_check), do_check);
+  gtk_toggle_button_set_active (GTK_TOGGLE_BUTTON (dialog->priv->test_check), do_check && gtk_widget_is_sensitive (dialog->priv->test_check));
 }
 
 void
